4fc927e8f8f95bf74858e0e5c0bf6e76f2f8c86c
PDOHO <xrobertjohn01@gmail.com>
2026-06-17 07:31:04 +0800

    Delete app directory

--- app/applet/test_export.js
+++
@@ -1,7 +0,0 @@
-import http from 'http';
-
-http.get('http://0.0.0.0:3000/api/export-coverage?year=2026', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log(data.substring(0, 500)));
-});
